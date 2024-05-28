91f20e36b003a84df6d21764558b467903952ea3
Yash Singh <singhyash3012@gmail.com>
2024-05-28 14:44:00 +0530

    Add files via upload

--- models/OwenerModel.js
+++ models/OwenerModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const OwenersSchema = new mongoose.Schema({
      fullname: {type: String, required: true},
-     email:{type:String, required:true},
+     email: {type: String, required:true},
      password: {type: Number, required: true},
 });
 
--- routes/OwenerRouter.js
+++ routes/OwenerRouter.js
@@ -4,23 +4,25 @@ const OwenersModel = require("../models/OwenerModel");
 const OwenersRouter = Router();
 
 OwenersRouter.post("/oweners",async(req,res)=>{
-     let oweners = await OwenersModel.find();
-     if(oweners>0){
-          return res.send(501).status("You don't have permission to create a New Owener");
+     let owenerscheck = await OwenersModel.find({});
+     if(owenerscheck.length>0){
+          return res.status(501).send("You don't have permission to create a New Owener");
+     }else{
+          const {fullname, email, password} = req.body;
+          console.log(req.body);
+          try{
+               const oweners = new OwenersModel({fullname, email, password});
+               await oweners.save();
+               res.status(201).json({
+                    message: "owener registered successfully"
+               });
+          }catch(error){
+               res.status(500).json({
+                    message: "Error creating New Owener", error
+               });
+               console.log(error);
+          };
      }
-     const {fullname, email, password} = req.body;
-     console.log(req.body);
-     try{
-          const oweners = new OwenersModel({fullname, email, password});
-          await oweners.save();
-          res.status(201).json({
-               message: "owener registered successfully"
-          });
-     }catch(error){
-          res.status(500).json({
-               message: "Error creating product", error
-          });
-     };
 });
 
 module.exports = OwenersRouter;
